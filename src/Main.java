acb16635dd20bf12c69523891e575af8a89e34a6
dfvarnell <dfvarnell@comcast.net>
2023-01-18 10:21:39 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("In vehicles main");
+        System.out.println("This is the second line");
+    }
+
+}
